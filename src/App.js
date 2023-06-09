1ccdb5a8feefdf64cccecfc8e9e803f2b99dbfd0
Austin Henrie <nitsua15@gmail.com>
2023-06-09 04:03:51 -0600

    working game

--- src/App.js
+++ src/App.js
@@ -2,11 +2,45 @@ import { useState } from 'react'
 import './App.css';
 import Square from "./Square"
 function App() {
+  function handleClick() {
+    setSquares(["", "", "", "", "", "", "", "", ""])
+    player[1](true)
+  }
+  function calculateWinner(arr) {
+    let lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    for (let i in lines) {
+      let [a, b, c] = lines[i]
+      console.log("got this far")
+      if (arr[a] && arr[a] === arr[b] && arr[a] === arr[c]) {
+        return arr[a] + " won."
+      }
+    }
+    return "Who will win?"
+  }
   let player = useState(true)
   let [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""])
   return (
     <div className="App">
-      <Square player = {player[0]} setPlayer = {player[1]} setSquares = {setSquares} squares = {squares} />
+      <span>
+        {calculateWinner(squares)}
+      </span>
+      <div className = "container">
+        {squares.map(function(value, index) {
+          return <Square index = {index} player = {player[0]} setPlayer = {player[1]} setSquares = {setSquares} squares = {squares} squareValue = {value} />
+        })}
+      </div>
+      <button onClick = {handleClick}>
+        Reset
+      </button>
     </div>
   );
 }
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,8 +1,18 @@
 import React from "react"
 let Square = (props) => {
-    console.log(props.squares, props.player)
-    return (
-        <div className = "square" />
-    )
+    function handleClick() {
+        if (!props.squareValue) {
+            if (props.player) {
+                props.squares[props.index] = "X"
+            } else {
+                props.squares[props.index] = "O"
+            }
+            props.setSquares(props.squares)
+            props.setPlayer(!props.player)
+        }
+    }
+    return <div className = "square" onClick = {handleClick}>
+        {props.squareValue === "O" ? <img alt = "" src = "https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" /> : props.squareValue}
+    </div>
 }
 export default Square
